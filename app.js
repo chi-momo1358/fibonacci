fc999aa36fbc8e0db7a1c6423e12dbd94d7c18ac
chi-momo1358 <iwasachi0618@gmail.com>
2025-01-23 15:04:01 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 'use strict';
+function fib(n) {
+  // フィボナッチ数列
+  // 0番目は 0 を返すルール
+  if (n === 0) return 0
+  // 1番目は 1 を返すルール
+  if (n === 1) return 1
+  // n番目の値を求める
+  return fib(n - 1) + fib(n - 2)
+}
+
+const length = 40
+for (let i = 0; i <= length; i++) {
+  console.log(fib(i))
+}
